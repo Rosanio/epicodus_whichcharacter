f93100a2a093be92f0f5147307557d28c4b94a68
Daren Schaad and Jason Awbrey <darenschaad@gmail.com>
2016-01-14 16:25:24 -0800

    Add an image with final answer

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var college = "Are you in college?";
+var college = "Did you graduate from college?";
 var inShape = "Are you in shape?";
 var dumpster = "Would you eat from the dumpster?"
 var narcissistic = "Are you all you can think about?"
@@ -19,6 +19,7 @@ $(function() {
     currentQuestion = college;
     $('#questionArea').text(currentQuestion);
     $('#character').empty();
+    $('img').remove();
   })
 });
 
@@ -31,16 +32,21 @@ function checkState(yesNo) {
       currentQuestion = narcissistic;
     } else if (currentQuestion === narcissistic) {
       $('#character').text("You are Deandra!");
+      $('.col-md-12').find('br').last().before("<img src='images/dee.jpg'></img>");
     } else {
       $('#character').text("You are Charlie!");
+      $('.col-md-12').find('br').last().before("<img src='images/charlie.jpg'></img>");
     }
   } else if (currentQuestion === inShape) {
         $('#character').text("You are Mac!");
+        $('.col-md-12').find('br').last().before("<img src='images/mac.jpg'></img>");
   } else if (currentQuestion === college) {
       currentQuestion = dumpster;
   } else if (currentQuestion === narcissistic) {
     $('#character').text("You are Dennis!");
+    $('.col-md-12').find('br').last().before("<img src='images/dennis.jpg'></img>");
   } else {
     $('#character').text("You are Frank!");
+    $('.col-md-12').find('br').last().before("<img src='images/frank.jpg'></img>");
   }
 }
